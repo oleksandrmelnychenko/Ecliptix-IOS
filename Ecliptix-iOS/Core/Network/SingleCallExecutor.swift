3928d2b97dc15f0dbf2a41af50c5d24faa782a15
Roman <ppgora352@gmail.com>
2025-06-09 05:01:52 -0700

    SingleCallExecutor initializator added

--- Ecliptix-iOS/Core/Network/SingleCallExecutor.swift
+++ Ecliptix-iOS/Core/Network/SingleCallExecutor.swift
@@ -5,3 +5,14 @@
 //  Created by Oleksandr Melnechenko on 06.06.2025.
 //
 
+public final class SingleCallExecutor {
+    let membershipServicesClient: Ecliptix_Proto_Membership_MembershipServices.ClientProtocol
+    let appDeviceServiceActionsClient: Ecliptix_Proto_AppDevice_AppDeviceServiceActions.ClientProtocol
+    let authenticationServicesClient: Ecliptix_Proto_Membership_AuthVerificationServices.ClientProtocol
+    
+    init(membershipServicesClient: Ecliptix_Proto_Membership_MembershipServices.ClientProtocol, appDeviceServiceActionsClient: Ecliptix_Proto_AppDevice_AppDeviceServiceActions.ClientProtocol, authenticationServicesClient: Ecliptix_Proto_Membership_AuthVerificationServices.ClientProtocol) {
+        self.membershipServicesClient = membershipServicesClient
+        self.appDeviceServiceActionsClient = appDeviceServiceActionsClient
+        self.authenticationServicesClient = authenticationServicesClient
+    }
+}
