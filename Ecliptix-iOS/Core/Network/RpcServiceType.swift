0f0bba4c9f7dc0948e6c035b473b48d20abde2f8
Roman <ppgora352@gmail.com>
2025-07-04 06:45:49 -0700

    improved retry logic + refactored Rpc Invokers

--- Ecliptix-iOS/Core/Network/RpcServiceType.swift
+++ Ecliptix-iOS/Core/Network/RpcServiceType.swift
@@ -5,7 +5,7 @@
 //  Created by Oleksandr Melnechenko on 06.06.2025.
 //
 
-public enum RcpServiceType {
+public enum RpcServiceType {
     case establishSecrecyChannel
     case restoreSecrecyChannel
     case registerAppDevice
