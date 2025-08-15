5eab0583a136344d7f88fed19eafdb41b422adbc
Roman <ppgora352@gmail.com>
2025-08-15 15:38:23 +0300

    feat: extend ChatMessage model to support reply references

--- Ecliptix-iOS/Views/Core/Models/ChatMessage.swift
+++ Ecliptix-iOS/Views/Core/Models/ChatMessage.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+struct ReplyRef: Identifiable, Equatable {
+    let id: UUID
+    let author: String
+    let text: String
+}
+
 enum MessageStatus {
     case sending
     case sent
@@ -23,14 +29,17 @@ struct ChatMessage: Identifiable, Equatable {
     var createdAt: Date
     var updatedAt: Date?
     var status: MessageStatus
-
+    
+    var replyTo: ReplyRef? = nil
+    
     init(
         id: UUID = UUID(),
         text: String,
         isSentByUser: Bool,
         createdAt: Date = Date(),
         updatedAt: Date? = nil,
-        status: MessageStatus = .sending
+        status: MessageStatus = .sending,
+        replyTo: ReplyRef? = nil
     ) {
         self.id = id
         self.text = text
@@ -38,6 +47,7 @@ struct ChatMessage: Identifiable, Equatable {
         self.createdAt = createdAt
         self.updatedAt = updatedAt
         self.status = status
+        self.replyTo = replyTo
     }
     
     static func ==(lhs: ChatMessage, rhs: ChatMessage) -> Bool {
